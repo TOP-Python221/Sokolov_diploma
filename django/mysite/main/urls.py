53cfa4e2b4a1a537904fef2f4381a44088135aec
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2023-04-18 23:25:00 +0800

    первый вывод на сайте

--- django/mysite/main/urls.py
+++ django/mysite/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('',views.main_viev)
+    path('',views.main_viev),# проверяет адрес на пустую строку и проверяет следующий адрес
+    path('about',views.about_view) #подключили еще одну страницу и указали для нее путь
 ]
--- django/mysite/main/views.py
+++ django/mysite/main/views.py
@@ -5,5 +5,10 @@ from django.shortcuts import render
 #здесь отображается видимое избражение в браузере
 
 def main_viev(request):
-    return HttpResponse('This my site'
-                        'fucking shit')
+    return HttpResponse('<h1>This my site\n<h1>'
+                        '<p>fucking shit<p>')
+
+
+def about_view(request):
+    #   передаем объект request и отностельный путь до страницы
+    return render(request, 'main/about.html')
